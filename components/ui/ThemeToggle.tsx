87f9785f7d84132bf086d288e7f52ff89767ec38
rc4317809 <rc4317809@gmail.com>
2024-07-28 00:40:31 +0800

    commit

--- components/ui/ThemeToggle.tsx
+++ components/ui/ThemeToggle.tsx
@@ -4,17 +4,23 @@ import { CiDark } from "react-icons/ci";
 
 const ThemeToggle = () => {
     const [dark, setDarkMode] = useState(() => {
-        // Use a function to set initial state to avoid unnecessary rendering
-        const theme = localStorage.getItem("theme");
-        return theme === "dark" ? true : false;
+        if (typeof window !== 'undefined') {
+            // Check localStorage only in the browser
+            const theme = localStorage.getItem("theme");
+            return theme === "dark";
+        }
+        // Default to light theme if window is not defined
+        return false;
     });
 
     useEffect(() => {
         // Apply the initial theme
-        if (dark) {
-            document.documentElement.classList.add('dark');
-        } else {
-            document.documentElement.classList.remove('dark');
+        if (typeof window !== 'undefined') {
+            if (dark) {
+                document.documentElement.classList.add('dark');
+            } else {
+                document.documentElement.classList.remove('dark');
+            }
         }
     }, [dark]);
 
@@ -24,15 +30,14 @@ const ThemeToggle = () => {
 
     // Sync theme with localStorage
     useEffect(() => {
-        localStorage.setItem("theme", dark ? "dark" : "light");
+        if (typeof window !== 'undefined') {
+            localStorage.setItem("theme", dark ? "dark" : "light");
+        }
     }, [dark]);
-
-    // Avoid rendering until the theme state is initialized
-    if (dark === null) return null;
 
     return (
         <div 
-            className="rounded-full p-2 bg-slate-300 dark:bg-slate-700 text-4xl text-gray-700 dark:text-slate-200 hover:text-orange-100 dark:hover:text-slate-900 cursor-pointer " 
+            className="rounded-full p-2 bg-slate-300 dark:bg-slate-700 text-4xl text-gray-700 dark:text-slate-200 hover:text-orange-100 dark:hover:text-slate-900 cursor-pointer" 
             onClick={handleToggle}>
             {dark ? <CiDark /> : <AiOutlineSun />}
         </div>
